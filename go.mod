2658dfeaca5c51a38c5e53926c96d8beba5c57ef
Carson Call <carson@callmail.net>
2024-04-15 22:23:49 -0600

    implement data structures and basic layout of MVP

---
+++ go.mod
@@ -0,0 +1,8 @@
+module example.com/m/v2
+
+go 1.22.1
+
+require (
+	github.com/neo4j/neo4j-go-driver/v5 v5.19.0
+	github.com/smacker/go-tree-sitter v0.0.0-20240402012804-99ab967cf9b9
+)
--- main.go
+++ main.go
@@ -4,45 +4,65 @@ import (
 	"context"
 	"fmt"
 	"os"
+	"path/filepath"
 
 	"github.com/neo4j/neo4j-go-driver/v5/neo4j"
+	sitter "github.com/smacker/go-tree-sitter"
+	"github.com/smacker/go-tree-sitter/typescript/typescript"
 )
 
-func helloWorld(ctx context.Context, uri, username, password string) (string, error) {
-	driver, err := neo4j.NewDriverWithContext(uri, neo4j.BasicAuth(username, password, ""))
+type Giraph struct {
+	Ctx    context.Context
+	DbConn neo4j.SessionWithContext
+	Parser sitter.Parser
+}
+
+func BirthGiraph(ctx context.Context, uri, username, password string) (*Giraph, error) {
+	connection, err := Connect(ctx, uri, username, password)
+	parser := sitter.NewParser()
+	parser.SetLanguage(typescript.GetLanguage())
 	if err != nil {
-		return "", err
+		fmt.Printf("failed to build Giraph: %s", err)
 	}
+	return &Giraph{
+		Ctx:    ctx,
+		DbConn: connection,
+		Parser: *parser,
+	}, nil
+}
+
+func Connect(ctx context.Context, uri, username, password string) (neo4j.SessionWithContext, error) {
+	driver, err := neo4j.NewDriverWithContext(uri, neo4j.BasicAuth(username, password, ""))
 	defer driver.Close(ctx)
 
 	session := driver.NewSession(ctx, neo4j.SessionConfig{AccessMode: neo4j.AccessModeWrite})
 	defer session.Close(ctx)
 
-	greeting, err := session.ExecuteWrite(ctx, func(transaction neo4j.ManagedTransaction) (any, error) {
-		result, err := transaction.Run(ctx,
-			"CREATE (a:Greeting) SET a.message = $message RETURN a.message + ', from node ' + id(a)",
-			map[string]any{"message": "hello, world"})
-		if err != nil {
-			return nil, err
-		}
+	// greeting, err := session.ExecuteWrite(ctx, func(transaction neo4j.ManagedTransaction) (any, error) {
+	// 	result, err := transaction.Run(ctx,
+	// 		"CREATE (a:Greeting) SET a.message = $message RETURN a.message + ', from node ' + id(a)",
+	// 		map[string]any{"message": "hello, world"})
+	// 	if err != nil {
+	// 		return nil, err
+	// 	}
 
-		if result.Next(ctx) {
-			return result.Record().Values[0], nil
-		}
+	// 	if result.Next(ctx) {
+	// 		return result.Record().Values[0], nil
+	// 	}
 
-		return nil, result.Err()
-	})
+	// 	return nil, result.Err()
+	//})
 	if err != nil {
-		return "failed to connect to database", err
+		fmt.Printf("failed to connect to Neo4j with this error: %s", err)
+		return nil, err
 	}
-
-	return greeting.(string), nil
+	return session, nil
 }
 
-func parse(path string, info os.FileInfo, err error) error {
-	if error != nil {
-		fmt.Print("Error accessing filepath %s: %v\n", path, error)
-		return nil
+func parse(giraph Giraph, path string, info os.FileInfo, err error) error {
+	if err != nil {
+		fmt.Printf("Error accessing filepath %s: %v\n", path, err)
+		return err
 	}
 
 	if info.IsDir() {
@@ -50,13 +70,28 @@ func parse(path string, info os.FileInfo, err error) error {
 	}
 
 	fmt.Printf("Parsing file %s\n", path)
-	
+	file, err := os.ReadFile(path)
+	tree, err := giraph.Parser.ParseCtx(giraph.Ctx, nil, file)
+	if err != nil {
+		fmt.Printf("Failed to parse file: %s", err)
+		return err
+	}
+
+	return nil
 }
 
 func main() {
 	fmt.Println("Hello, World!")
 	ctx := context.Background()
-	var path String := os.Args[1]
-	err := filepath.walk(path, )
-	fmt.Println(helloWorld(ctx, "bolt://localhost:7687", "neo4j", "2ShutYoTrap"))
+	var path string = os.Args[1]
+
+	giraph, err := BirthGiraph(ctx, "bolt://localhost:7687", "neo4j", "2ShutYoTrap")
+	if err != nil {
+		fmt.Print(err)
+	}
+
+	parseFunc := func(path string, info os.FileInfo, err error) error {
+		return parse(*giraph, path, info, err)
+	}
+	filepath.Walk(path, parseFunc)
 }
