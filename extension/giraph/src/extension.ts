de3aeaff2fb20e74c8c4ad98d44fb67b87e91ead
davidthompson <matkatdavid@gmail.com>
2024-05-13 09:53:11 -0600

    boilerplate for extension

--- extension/giraph/src/extension.ts
+++ extension/giraph/src/extension.ts
@@ -1,25 +1,38 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+import { getWebviewOptions, GiraphPanel } from "./GiraphPanel";
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  context.subscriptions.push(
+    vscode.commands.registerCommand("giraph.start", () => {
+      GiraphPanel.createOrShow(context.extensionUri);
+    })
+  );
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "giraph" is now active!');
+  context.subscriptions.push(
+    vscode.commands.registerCommand("giraph.doRefactor", () => {
+      if (GiraphPanel.currentPanel) {
+        GiraphPanel.currentPanel.doRefactor();
+      }
+    })
+  );
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('giraph.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Giraph!');
-	});
-
-	context.subscriptions.push(disposable);
+  if (vscode.window.registerWebviewPanelSerializer) {
+    // make sure we register a serializer in activation event
+    vscode.window.registerWebviewPanelSerializer(GiraphPanel.viewType, {
+      async deserializeWebviewPanel(
+        webviewPanel: vscode.WebviewPanel,
+        state: any
+      ) {
+        console.log(`Got state: ${state}`);
+        webviewPanel.webview.options = getWebviewOptions(context.extensionUri);
+        GiraphPanel.revive(webviewPanel, context.extensionUri);
+      },
+    });
+  }
 }
 
 // This method is called when your extension is deactivated
