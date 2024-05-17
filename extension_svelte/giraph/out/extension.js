c5b740d1f561511ae11ea7ee0ec5d5b45f13fda1
davidthompson <matkatdavid@gmail.com>
2024-05-17 13:35:14 -0600

    make changes

---
+++ extension_svelte/giraph/out/extension.js
@@ -0,0 +1,29 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from "vscode";
+import { getWebviewOptions, GiraphPanel } from "./GiraphPanel.js";
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
+export function activate(context) {
+    context.subscriptions.push(vscode.commands.registerCommand("giraph.start", () => {
+        GiraphPanel.createOrShow(context.extensionUri);
+    }));
+    context.subscriptions.push(vscode.commands.registerCommand("giraph.doRefactor", () => {
+        if (GiraphPanel.currentPanel) {
+            GiraphPanel.currentPanel.doRefactor();
+        }
+    }));
+    if (vscode.window.registerWebviewPanelSerializer) {
+        // make sure we register a serializer in activation event
+        vscode.window.registerWebviewPanelSerializer(GiraphPanel.viewType, {
+            async deserializeWebviewPanel(webviewPanel, state) {
+                console.log(`Got state: ${state}`);
+                webviewPanel.webview.options = getWebviewOptions(context.extensionUri);
+                GiraphPanel.revive(webviewPanel, context.extensionUri);
+            },
+        });
+    }
+}
+// This method is called when your extension is deactivated
+export function deactivate() { }
+//# sourceMappingURL=extension.js.map
